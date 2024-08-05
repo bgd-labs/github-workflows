31153acb8f32b9574d79439f4ee5d4ca8aa4aa90
Lukas <lukasstrassel@googlemail.com>
2024-08-05 23:05:20 +0200

    feat: install zksync (#28)

--- src/test/Test.t.sol
+++ src/test/Test.t.sol
@@ -1,3 +1,8 @@
-contract T {
-    
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TestB {
+    function test_c() external pure returns (uint256) {
+        return 1;
+    }
 }
