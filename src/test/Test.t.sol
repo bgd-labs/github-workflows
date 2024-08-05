aa853d98d6ec031b984fac97cd5b492de1b6a7f1
sakulstra <lukasstrassel@googlemail.com>
2024-08-05 22:52:12 +0200

    fix: add underscore

--- src/test/Test.t.sol
+++ src/test/Test.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract TestB {
-    function testContract() external pure returns (uint256) {
+    function test_Contract() external pure returns (uint256) {
         return 1;
     }
 }
