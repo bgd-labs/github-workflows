165deb2603cb85b97259acd94ac6b066ebd353fc
sakulstra <lukasstrassel@googlemail.com>
2024-08-05 22:45:32 +0200

    fix: use same contract as harsh

--- src/test/Test.t.sol
+++ src/test/Test.t.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract TestB {
-    function test_b() external pure returns (uint256) {
-        return 1;
-    }
+contract TestContract {
+    function testContract() external pure {}
 }
